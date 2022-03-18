11fa658757bbeeeec618d5f811280ed13f42ecb6
Panat Taranat <panat.taranat@gmail.com>
2022-03-18 16:45:57 -0400

    fish: add fzf options, use exa, search hidden files

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,8 @@
 set fish_greeting
 set -gx GPG_TTY (tty)
+set -gx FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
+
+set fzf_fd_opts --hidden --exclude=.git
+set fzf_preview_dir_cmd exa --all --color=always
 
 zoxide init --cmd j fish | source
