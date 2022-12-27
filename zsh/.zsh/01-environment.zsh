dae7dea7213b6b2f3ae8ae2bed89285926c8e4d0
Panat Taranat <panat.taranat@gmail.com>
2022-12-27 13:58:21 +0700

    zsh: use double quotes

--- zsh/.zsh/01-environment.zsh
+++ zsh/.zsh/01-environment.zsh
@@ -38,7 +38,7 @@ eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
 
 # fzf + rg
-export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
+export FZF_DEFAULT_COMMAND="rg --files --no-ignore --hidden --follow -g '!{.git,node_modules}/*' 2> /dev/null"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 # NVM
