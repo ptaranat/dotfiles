2704ebac71d1c9f81b0d08ac3e03587b514e3cfd
Panat Taranat <panat.taranat@gmail.com>
2026-03-12 23:17:10 -0400

    rpg: remove roll

--- zsh/.zsh/07-rpg.zsh
+++ zsh/.zsh/07-rpg.zsh
@@ -1,7 +1,3 @@
-# source /Users/panat/projects/rpg/roll/bash_completion/roll
-znap install matteocorti/roll
-source ~[roll]/bash_completion/roll
-
 # Use uv script for dice rolling
 alias dice='uv run ~/.dotfiles/zsh/.zsh/rpg.py dice'
 alias thaco='uv run ~/.dotfiles/zsh/.zsh/rpg.py thaco'
